f9e9700e12b7d403604229cf44ae9c89a6dc5855
leandro balmaceda <leandro20.balmaceda@gmail.com>
2023-08-09 11:22:40 -0300

    prueba

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 style={{fontSize:"2rem",textAlign:"center"}}>Hola soy archivo jsx y estoy funcionado gracias un docker Compose :D</h1>
+    </>
+  )
+}
+
+export default App
